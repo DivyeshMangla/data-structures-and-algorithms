feb5a04b16ec03da01d2687e76dd813c1c32def3
Divyesh Mangla <174847645+DivyeshMangla@users.noreply.github.com>
2025-11-05 20:24:47 +0530

    leetcode: solved two_sum in go

--- leetcode/go/two_sum.go
+++ leetcode/go/two_sum.go
@@ -1,1 +1,26 @@
-package _go
+package main
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	for _, v := range twoSum(nums, target) {
+		println(v)
+	}
+}
+
+func twoSum(nums []int, target int) []int {
+	appeared := make(map[int]int)
+
+	for i1, num1 := range nums {
+		needed := target - num1
+
+		if i2, ok := appeared[needed]; ok && i1 != i2 {
+			return []int{i1, i2}
+		}
+
+		appeared[num1] = i1
+	}
+
+	return []int{}
+}
